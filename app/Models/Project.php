3d0cb1f3496c627197625985ec16fd58e1146cd9
LMartelini <laismartelini03@gmail.com>
2026-02-28 09:30:57 -0300

    feat: add relationships and model configuration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'due_date' => 'date',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
