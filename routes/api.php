45bb9601688489a1e7eb6a7160aa54066bdbe46d
LMartelini <laismartelini03@gmail.com>
2026-03-01 08:34:58 -0300

    feat: apply api rate limiting to all endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
+    use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\Api\ProjectController;
     use App\Http\Controllers\Api\TaskController;
 
-    Route::get('/projects', [ProjectController::class, 'index']);
-    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::middleware('throttle:api')->group(function () {
+        Route::apiResource('projects', ProjectController::class)->only([
+            'index', 'store'
+        ]);
 
-    Route::get('/projects/{project}/tasks', [TaskController::class, 'index']);
-    Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
-
-    Route::patch('/tasks/{task}', [TaskController::class, 'update']);
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+        Route::get('projects/{project}/tasks', [TaskController::class, 'index']);
+        Route::post('projects/{project}/tasks', [TaskController::class, 'store']);
+        Route::patch('tasks/{task}', [TaskController::class, 'update']);
+        Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
+    });
