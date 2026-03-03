aef399ee6ec94649e1c34a5ed6da9a7b47eacaa1
LMartelini <laismartelini03@gmail.com>
2026-03-03 18:09:13 -0300

    feat: implement project detail view with API integration

--- frontend/src/views/ProjectDetailView.vue
+++ frontend/src/views/ProjectDetailView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-defineProps<{ id: string }>()
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { useProject } from '@/composables/useProject'
+
+const route = useRoute()
+const { project, loading, error, fetchProject } = useProject()
+
+onMounted(() => {
+	fetchProject(Number(route.params.id))
+})
 </script>
 
 <template>
-  <div>
-    <h1>Detalhe do Projeto</h1>
-  </div>
+	<div>
+		<p v-if="loading">Carregando...</p>
+		<p v-else-if="error">{{ error }}</p>
+
+		<div v-else-if="project">
+			<h1>{{ project.name }}</h1>
+			<p>{{ project.description }}</p>
+			<p>Status: {{ project.status }}</p>
+			<p>Tarefas: {{ project.tasks_count }}</p>
+		</div>
+	</div>
 </template>
