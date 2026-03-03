1b078756b3f5a1853e103b6ea0cdb0e1d5cf9930
LMartelini <laismartelini03@gmail.com>
2026-03-03 17:52:06 -0300

    feat: implement useProject composable

---
+++ frontend/src/composables/UserProject.ts
@@ -0,0 +1,28 @@
+import { ref } from 'vue'
+import { projectsService, type Project } from '@/services/projects.service'
+
+export function useProject() {
+  const project = ref<Project | null>(null)
+  const loading = ref(false)
+  const error = ref<string | null>(null)
+
+  const fetchProject = async (id: number) => {
+    try {
+      loading.value = true
+      error.value = null
+
+      project.value = await projectsService.getById(id)
+    } catch {
+      error.value = 'Erro ao carregar projeto'
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    project,
+    loading,
+    error,
+    fetchProject,
+  }
+}
