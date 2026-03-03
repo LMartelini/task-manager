5ebc3eb067ca6aadd6a0b723024294dd20db6d60
LMartelini <laismartelini03@gmail.com>
2026-03-03 20:22:08 -0300

    feat: display project tasks in detail view

--- frontend/src/views/ProjectDetailView.vue
+++ frontend/src/views/ProjectDetailView.vue
@@ -2,9 +2,14 @@
 import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useProject } from '@/composables/useProject'
+import { useTasks } from '@/composables/useTasks'
+
 
 const route = useRoute()
 const { project, loading, error, fetchProject } = useProject()
+
+const { tasks, loading: tasksLoading, error: tasksError } =
+  useTasks(Number(route.params.id))
 
 onMounted(() => {
 	fetchProject(Number(route.params.id))
@@ -23,4 +28,22 @@ onMounted(() => {
 			<p>Tarefas: {{ project.tasks_count }}</p>
 		</div>
 	</div>
+
+	<h2>Tarefas</h2>
+
+	<p v-if="tasksLoading">Carregando tarefas...</p>
+	<p v-else-if="tasksError">{{ tasksError }}</p>
+
+	<ul v-else-if="tasks.length">
+		<li v-for="task in tasks" :key="task.id">
+			<strong>{{ task.title }}</strong>
+			- {{ task.status }}
+			- Prioridade: {{ task.priority }}
+			<span v-if="task.is_overdue" style="color: red;">
+			(Atrasada)
+			</span>
+		</li>
+	</ul>
+
+	<p v-else>Nenhuma tarefa encontrada.</p>
 </template>
